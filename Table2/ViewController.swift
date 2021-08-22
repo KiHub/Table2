fc2fd1de9b6bc65f28ca1381741dea375236a730
Kirill Ki <kirillkalapov@gmail.com>
2021-08-22 14:48:32 +0300

    Initial Commit

---
+++ Table2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Table2
+//
+//  Created by  Mr.Ki on 22.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
